c553e0ce9bb13871e37411112eb3d594e2c539a5
izzdanial99 <2100735@sit.singaporetech.edu.sg>
2025-05-04 18:46:03 +0800

    Fix typo

--- frontend/Hotel-booking-application/src/components/utils/ApiFunctions.js
+++ frontend/Hotel-booking-application/src/components/utils/ApiFunctions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_UR,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getHeader = () => {
