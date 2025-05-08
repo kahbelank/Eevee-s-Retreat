c2cf9907e39f27004c15d539ef4a0c49fa081ed4
Kahbelan <97492848+kahbelank@users.noreply.github.com>
2025-05-08 14:56:13 +0800

    Update StaticResourceConfig.java

--- backend/HotelBookingApplication/src/main/java/com/eevee/HotelBookingApplication/config/StaticResourceConfig.java
+++ backend/HotelBookingApplication/src/main/java/com/eevee/HotelBookingApplication/config/StaticResourceConfig.java
@@ -1,15 +1,18 @@
 package com.eevee.HotelBookingApplication.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/robots.txt", "/sitemap.xml")
+        registry.addResourceHandler("/robots.txt", "/sitemap.xml", "/")
                 .addResourceLocations("classpath:/static/")
-                .setCachePeriod(3600); // 1 hour
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic());
     }
 }
